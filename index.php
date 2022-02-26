c49513ad1c8945a48ea1ebfba277d93cbaf52ae9
GOTAM GORABH <gautamy67@gmail.com>
2022-02-26 18:24:35 +0530

    My First Commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        
+        
+        
+        <?php
+        
+            echo '<h2><center>Welcome to Community Bank<center></h2>';
+            echo '<hr>';
+        
+        ?>
+    </body>
+</html>
